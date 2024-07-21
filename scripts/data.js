b36b5a731ae308d9a0f70b63004b131330bae847
unknown <hegde.advik1234@gmail.com>
2024-07-21 23:58:23 +0530

    Initial Files

---
+++ scripts/data.js
@@ -0,0 +1,386 @@
+export const data =[{
+    round:"10",
+    date:'07.06',
+
+    game1Player1:"Magnus Carlsen",
+    game1player1img:"small-images/magnus.jpeg",
+    game1score:"1.5 - 1",
+    game1Player2:"Fabiano Caruana",
+    game1player2img:"/small-images/fabi.jpeg",
+
+    game2Player1:"Praggnanandhaa Rameshbabu",
+    game2player1img:"small-images/pragg.jpeg",
+    game2score:"1.5 - 1",
+    game2player2img:"small-images/hikaru.jpeg",
+    game2Player2:"Hikaru Nakamura",
+
+    game3Player1:"Ding Liren",
+    game3player1img:"small-images/ding.jpeg",
+    game3score:"1 - 1.5",
+    game3Player2:"Alireza Firouza",
+    game3player2img:"small-images/alireza.jpeg",
+
+  },
+
+  {
+    round:"9",
+    date:'06.06',
+
+    game1Player1:"Alireza Firouza",
+    game1player1img:"small-images/alireza.jpeg",
+    game1score:"1 - 1.5",
+    game1Player2:"Magnus Carlsen",
+    game1player2img:"small-images/magnus.jpeg",
+
+    game2Player1:"Hikaru Nakamura",
+    game2player1img:"small-images/hikaru.jpeg",
+    game2score:"1 - 1.5",
+    game2Player2:"Ding Liren",
+    game2player2img:"small-images/ding.jpeg",
+
+    game3Player1:"Fabiano Caruana",
+    game3player1img:"../small-images/fabi.jpeg",
+    game3score:"1.5 - 1",
+    game3Player2:"Praggnanandhaa Rameshbabu",
+    game3player2img:"small-images/pragg.jpeg",
+  },
+  {
+    round:"8",
+    date:'04.06',
+
+    game1Player1:"Magnus Carlsen",
+    game1player1img:"/small-images/magnus.jpeg",
+    game1score:"1.5 - 1",
+    game1Player2:"Praggnanandhaa Rameshbabu",
+    game1player2img:"small-images/pragg.jpeg",
+
+    game2Player1:"Ding Liren",
+    game2player1img:"small-images/ding.jpeg",
+    game2score:"1 - 1.5",
+    game2Player2:"Fabiano Caruana",
+    game2player2img:"/small-images/fabi.jpeg",
+
+
+    game3Player1:"Alireza Firouza",
+    game3player1img:"small-images/alireza.jpeg",
+    game3score:"1.5 - 1",
+    game3Player2:"Hikaru Nakamura",
+    game3player2img:"small-images/hikaru.jpeg",
+
+  },
+  {
+    round:"7",
+    date:'03.06',
+
+    game1Player1:"Hikaru Nakamura",
+    game1player1img:"small-images/hikaru.jpeg",
+    game1score:"1.5 - 1",
+    game1Player2:"Magnus Carlsen",
+    game1player2img:"small-images/magnus.jpeg",
+
+    game2Player1:"Fabiano Caruana",
+    game2player1img:"/small-images/fabi.jpeg",
+    game2score:"1 - 1.5",
+    game2Player2:"Alireza Firouza",
+    game2player2img:"small-images/alireza.jpeg",
+
+    game3Player1:"Praggnanandhaa Rameshbabu",
+    game3player1img:"small-images/pragg.jpeg",
+    game3score:"1.5 - 1",
+    game3Player2:"Ding Liren",
+    game3player2img:"small-images/ding.jpeg",
+  },
+  {
+    round:"6",
+    date:'02.06',
+
+    game1Player1:"Magnus Carlsen",
+    game1player1img:"small-images/magnus.jpeg",
+    game1score:"3 - 0",
+    game1Player2:"Ding Liren",
+    game1player2img:"small-images/ding.jpeg",
+
+    game2Player1:"Alireza Firouza",
+    game2player1img:"small-images/alireza.jpeg",
+    game2score:"1.5 - 1",
+    game2Player2:"Praggnanandhaa Rameshbabu",
+    game2player2img:"small-images/pragg.jpeg",
+
+    game3Player1:"Hikaru Nakamura",
+    game3player1img:"small-images/hikaru.jpeg",
+    game3score:"1 - 1.5",
+    game3Player2:"Fabiano Caruana",
+    game3player2img:"/small-images/fabi.jpeg",
+  },
+  {
+    round:"5",
+    date:'31.05',
+
+    game1Player1:"Magnus Carlsen",
+    game1player1img:"small-images/magnus.jpeg",
+    game1score:"3 - 0",
+    game1Player2:"Alireza Firouza",
+    game1player2img:"small-images/alireza.jpeg",
+
+    game2Player1:"Ding Liren",
+    game2player1img:"small-images/ding.jpeg",
+    game2score:"0 - 3",
+    game2Player2:"Hikaru Nakamura",
+    game2player2img:"small-images/hikaru.jpeg",
+
+    game3Player1:"Praggnanandhaa Rameshbabu",
+    game3player1img:"small-images/pragg.jpeg",
+    game3score:"3 - 0",
+    game3Player2:"Fabiano Caruana",
+    game3player2img:"/small-images/fabi.jpeg",
+  },
+  {
+    round:"4",
+    date:'30.05',
+
+    game1Player1:"Fabiano Caruana",
+    game1player1img:"/small-images/fabi.jpeg",
+    game1score:"0 - 3",
+    game1Player2:"Magnus Carlsen",
+    game1player2img:"small-images/magnus.jpeg",
+
+    game2Player1:"Hikaru Nakamura",
+    game2player1img:"small-images/hikaru.jpeg",
+    game2score:"3 - 0",
+    game2Player2:"Praggnanandhaa Rameshbabu",
+    game2player2img:"small-images/hikaru.jpeg",
+
+    game3Player1:"Alireza Firouza",
+    game3player1img:"small-images/alireza.jpeg",
+    game3score:"3 - 0",
+    game3Player2:"Ding Liren",
+    game3player2img:"small-images/ding.jpeg",
+  },
+  {
+    round:"3",
+    date:'29.05',
+
+    game1Player1:"Praggnanandhaa Rameshbabu",
+    game1player1img:"small-images/pragg.jpeg",
+    game1score:"3 - 0",
+    game1Player2:"Magnus Carlsen",
+    game1player2img:"small-images/magnus.jpeg",
+
+    game2Player1:"Fabiano Caruana",
+    game2player1img:"../small-images/fabi.jpeg",
+    game2score:"3 - 0",
+    game2Player2:"Ding Liren",
+    game2player2img:"small-images/ding.jpeg",
+
+    game3Player1:"Hikaru Nakamura",
+    game3player1img:"small-images/hikaru.jpeg",
+    game3score:"1.5 - 1",
+    game3Player2:"Alireza Firouza",
+    game3player2img:"small-images/alireza.jpeg",
+  },
+  {
+    round:"2",
+    date:'28.05',
+
+    game1Player1:"Magnus Carlsen",
+    game1player1img:"small-images/magnus.jpeg",
+    game1score:"1.5 - 1",
+    game1Player2:"Hikaru Nakamura",
+    game1player2img:"small-images/hikaru.jpeg",
+
+    game2Player1:"Alireza Firouza",
+    game2player1img:"small-images/alireza.jpeg",
+    game2score:"1.5 - 1",
+    game2Player2:"Fabiano Caruana",
+    game2player2img:"/small-images/fabi.jpeg",
+
+    game3Player1:"Ding Liren",
+    game3player1img:"small-images/ding.jpeg",
+    game3score:"1.5 - 1",
+    game3Player2:"Praggnanandhaa Rameshbabu",
+    game3player2img:"small-images/pragg.jpeg",
+  },
+  {
+    round:"1",
+    date:'27.05',
+
+    game1Player1:"Ding Liren",
+    game1player1img:"small-images/ding.jpeg",
+    game1score:"1 - 1.5",
+    game1Player2:"Magnus Carlsen",
+    game1player2img:"small-images/magnus.jpeg",
+
+    game2Player1:"Praggnanandhaa Rameshbabu",
+    game2player1img:"small-images/pragg.jpeg",
+    game2score:"1.5 - 1",
+    game2Player2:"Alireza Firouza",
+    game2player2img:"small-images/alireza.jpeg",
+
+    game3Player1:"Fabiano Caruana",
+    game3player1img:"/small-images/fabi.jpeg",
+    game3score:"1 - 1.5",
+    game3Player2:"Hikaru Nakamura",
+    game3player2img:"small-images/hikaru.jpeg",
+  }
+]
+
+export let displayContent = ``;
+
+export let key=0;
+
+export function leftArrow(){
+
+  if(key<10){
+    key++;
+  }
+
+  
+
+  if(data[key]){
+    displayContent=`
+    <div class="round-info" >Round ${data[key].round}
+        <div class="left-arrow">&larr;</div>
+        <div class="right-arrow">&rarr;</div>
+        <div class="round-num">${data[key].date}</div>
+      </div>
+
+      <div class="match-1">
+        <div class="image-container-left">
+          <img class ="player-image-left" src="${data[key].game1player1img}" alt="">
+        </div>
+
+        <div class="deets">
+          <p class="player-name-left">${data[key].game1Player1}</p>
+          <p class="score">${data[key].game1score}</p>
+          <p class="player-name-right">${data[key].game1Player2}</p>
+        </div>
+        
+        <div class="image-container-right">
+          <img class="player-image-right" src="${data[key].game1player2img}" alt="">
+        </div>
+      </div>
+
+      <div>
+        <div class="match-1">
+          <div class="image-container-left">
+            <img class ="player-image-left" src="${data[key].game2player1img}" alt="">
+          </div>
+    
+          <div class="deets">
+            <p class="player-name-left">${data[key].game2Player1}</p>
+            <p class="score">${data[key].game2score}</p>
+            <p class="player-name-right">${data[key].game2Player2}</p>
+          </div>
+          
+    
+          <div class="image-container-right">
+            <img class="player-image-right" src="${data[key].game2player2img}" alt="">
+          </div>
+        </div>
+      </div>
+
+
+      <div>
+        <div class="match-1">
+          <div class="image-container-left">
+            <img class ="player-image-left" src="${data[key].game3player1img}" alt="">
+          </div>
+    
+          <div class="deets">
+            <p class="player-name-left">${data[key].game3Player1}</p>
+            <p class="score">${data[key].game3score}</p>
+            <p class="player-name-right">${data[key].game3Player2}</p>
+          </div>
+          
+    
+          <div class="image-container-right">
+            <img class="player-image-right" src="${data[key].game3player2img}" alt="">
+          </div>
+        </div>
+      </div>
+    `
+    document.querySelector('.main-container').innerHTML= displayContent;
+    document.querySelector('.left-arrow').addEventListener('click',()=>leftArrow()) 
+    document.querySelector('.right-arrow').addEventListener('click',()=>rightArrow()) 
+
+  } 
+}
+
+export function rightArrow(){
+  console.log(key);
+
+  if(key>0){
+    key--;
+  }
+
+  if(data[key]){
+    displayContent=`
+    <div class="round-info" >Round ${data[key].round}
+        <div class="left-arrow">&larr;</div>
+        <div class="right-arrow">&rarr;</div>
+        <div class="round-num">${data[key].date}</div>
+      </div>
+
+      <div class="match-1">
+        <div class="image-container-left">
+          <img class ="player-image-left" src="${data[key].game1player1img}" alt="">
+        </div>
+
+        <div class="deets">
+          <p class="player-name-left">${data[key].game1Player1}</p>
+          <p class="score">${data[key].game1score}</p>
+          <p class="player-name-right">${data[key].game1Player2}</p>
+        </div>
+        
+        <div class="image-container-right">
+          <img class="player-image-right" src="${data[key].game1player2img}" alt="">
+        </div>
+      </div>
+
+      <div>
+        <div class="match-1">
+          <div class="image-container-left">
+            <img class ="player-image-left" src="${data[key].game2player1img}" alt="">
+          </div>
+    
+          <div class="deets">
+            <p class="player-name-left">${data[key].game2Player1}</p>
+            <p class="score">${data[key].game2score}</p>
+            <p class="player-name-right">${data[key].game2Player2}</p>
+          </div>
+          
+    
+          <div class="image-container-right">
+            <img class="player-image-right" src="${data[key].game2player2img}" alt="">
+          </div>
+        </div>
+      </div>
+
+
+      <div>
+        <div class="match-1">
+          <div class="image-container-left">
+            <img class ="player-image-left" src="${data[key].game3player1img}" alt="">
+          </div>
+    
+          <div class="deets">
+            <p class="player-name-left">${data[key].game3Player1}</p>
+            <p class="score">${data[key].game3score}</p>
+            <p class="player-name-right">${data[key].game3Player2}</p>
+          </div>
+          
+    
+          <div class="image-container-right">
+            <img class="player-image-right" src="${data[key].game3player2img}" alt="">
+          </div>
+        </div>
+      </div>
+    `
+    document.querySelector('.main-container').innerHTML= displayContent;
+    document.querySelector('.left-arrow').addEventListener('click',()=>leftArrow()) 
+    document.querySelector('.right-arrow').addEventListener('click',()=>rightArrow()) 
+  }
+}
+
+
+ 
